2081623233289dabfe3dbd447b263bd987fd2ff2
shresta <shresta0912@gmail.com>
2021-07-04 16:46:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tester/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tester;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button1);
+        btn.setOnClickListener(v -> openactivity2());
+    }
+    public void openactivity2(){
+        Intent act = new Intent(this, Activity2.class);
+        startActivity(act);
+    }
+}
